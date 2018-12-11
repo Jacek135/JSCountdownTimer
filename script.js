12c6d54a92c50d72e6a89dd74c9b2027be7729fe
Jacek135 <jac.poczatek@gmail.com>
2018-12-11 21:58:35 +0100

    CountDown Variables

---
+++ script.js
@@ -0,0 +1,8 @@
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+const clock = document.querySelector('#clock');
+const daysSpan = clock.querySelector('.days');
+const hoursSpan = clock.querySelector('.hours');
+const minutesSpan = clock.querySelector('.minutes');
+const secondsSpan = clock.querySelector('.seconds');
