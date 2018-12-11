7989405c7bec8236908edd1c739de7f203d95998
Jacek135 <jac.poczatek@gmail.com>
2018-12-11 22:03:25 +0100

    Add EventListener

--- script.js
+++ script.js
@@ -6,3 +6,11 @@ const daysSpan = clock.querySelector('.days');
 const hoursSpan = clock.querySelector('.hours');
 const minutesSpan = clock.querySelector('.minutes');
 const secondsSpan = clock.querySelector('.seconds');
+
+endDate.addEventListener('change', function(e) {
+   e.preventDefault();
+   clearInterval(timeInterval);
+   console.dir(this);
+   const endDate = new Date(this.value);
+
+});
